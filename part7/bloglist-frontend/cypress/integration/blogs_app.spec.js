e54fe60e24ed4e76330b94b78bcf81104597a516
Orhan Ugurlu <orhan.ugurlu@gmail.com>
2020-10-29 01:07:48 +0100

    Updated cypress tests for part 7 exercises

--- part7/bloglist-frontend/cypress/integration/blogs_app.spec.js
+++ part7/bloglist-frontend/cypress/integration/blogs_app.spec.js
@@ -10,6 +10,18 @@ const testUser2 = {
   password: 'password2',
 }
 
+const testBlog1 = {
+  title: 'React patterns',
+  author: 'Michael Chan',
+  url: 'https://reactpatterns.com/',
+}
+
+const testBlog2 = {
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+}
+
 describe('Blog list app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
@@ -23,102 +35,210 @@ describe('Blog list app', function () {
   })
 
   it('front page can be opened', function () {
-    cy.contains('Blog List Application')
-    cy.contains('Login to application')
-    cy.get('#loginform')
-    cy.get('input[name="Username"]')
-    cy.get('input[name="Password"]')
-    cy.contains('Blogs')
+    cy.contains('Blog List Application').should('be.visible')
+    cy.contains('Welcome to blog application!').should('be.visible')
+    cy.contains('a', 'Home').should('have.attr', 'href', '/').should('be.visible')
+    cy.contains('a', 'Blogs').should('have.attr', 'href', '/blogs').should('be.visible')
+    cy.contains('a', 'Users').should('have.attr', 'href', '/users').should('be.visible')
+    cy.contains('a', 'Login').should('have.attr', 'href', '/login').should('be.visible')
+  })
+
+  it('create new blog is not possible when not logged in', function () {
+    cy.contains('a', 'Blogs').click()
+    cy.contains('button', 'Create new').should('not.be.visible')
+  })
+
+  describe('Blogs', function () {
+    beforeEach(function () {
+      cy.login({ username: testUser.username, password: testUser.password })
+      cy.createBlog(testBlog1)
+      cy.login({ username: testUser2.username, password: testUser2.password })
+      cy.createBlog(testBlog2)
+      cy.login({ username: testUser.username, password: testUser.password })
+      cy.contains('a', 'Blogs').click()
+    })
+
+    it('blogs are listed', function () {
+      cy.contains('Blogs').should('be.visible')
+      cy.contains(testBlog1.title).should('be.visible')
+      cy.contains(testBlog2.title).should('be.visible')
+      cy.contains('button', 'Create new').should('be.visible')
+    })
+
+    it('clicked blog is displayed', function () {
+      cy.contains(testBlog1.title).click()
+      cy.contains(`Blog: '${testBlog1.title}' by '${testBlog1.author}'`).should('be.visible')
+      cy.contains('0').should('be.visible')
+      cy.contains(testBlog1.url).should('be.visible')
+      cy.contains(testUser.name).should('be.visible')
+    })
+
+    it('clicking like button increases number of likes', function () {
+      cy.contains(testBlog1.title).click()
+      cy.get('button').contains('Like').click()
+      cy.contains('1').should('be.visible')
+      cy.get('button').contains('Like').click()
+      cy.contains('2').should('be.visible')
+    })
+
+    it('clicking owner of blog displays user', function () {
+      cy.contains(testBlog1.title).click()
+      cy.get('a').contains(testUser.name).click()
+      cy.contains(`User: ${testUser.name}`).should('be.visible')
+      cy.contains(testBlog1.title).should('be.visible')
+    })
   })
 
   describe('Login', function () {
+    beforeEach(function () {
+      cy.contains('a', 'Login').click()
+      cy.contains('Login to application').should('be.visible')
+    })
+
+    it('login form displayed', function () {
+      cy.get('input[name="username"]').should('exist')
+      cy.get('input[name="password"]').should('exist')
+      cy.get('form').contains('Login').should('be.visible')
+      cy.get('form').contains('Cancel').should('be.visible')
+    })
+
     it('succeeds with correct credentials', function () {
-      cy.get('input[name="Username"]').type(testUser.username)
-      cy.get('input[name="Password"]').type(testUser.password)
-      cy.get('#loginbutton').click()
-      cy.contains(`${testUser.name} logged in`)
-      cy.get('#loginform').should('not.exist')
+      cy.get('input[name="username"]').type(testUser.username)
+      cy.get('input[name="password"]').type(testUser.password)
+      cy.get('form').contains('Login').click()
+      cy.contains(`${testUser.name} logged in`).should('be.visible')
+      cy.get('form').should('not.exist')
+      cy.contains('Logout').should('be.visible')
     })
 
     it('fails with wrong credentials', function () {
-      cy.get('input[name="Username"]').type(testUser.username)
-      cy.get('input[name="Password"]').type(testUser.password + 'dummy')
-      cy.get('#loginbutton').click()
-      cy.get('.error').contains('Wrong credentials!')
-      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
-      cy.get('#loginform')
-      cy.wait(3100)
-      cy.get('.error').should('not.exist')
+      cy.get('input[name="username"]').type(testUser.username)
+      cy.get('input[name="password"]').type(testUser.password + 'dummy')
+      cy.get('form').contains('Login').click()
+      cy.contains('Wrong credentials!').should('be.visible')
+      cy.get('form').should('exist')
+      cy.wait(5100)
+      cy.contains('Wrong credentials!').should('not.be.visible')
     })
   })
 
-  describe.only('When logged in', function () {
+  describe('When logged in', function () {
     beforeEach(function () {
       cy.login({ username: testUser.username, password: testUser.password })
     })
 
     it('A blog can be created', function () {
-      cy.contains('New Blog').click()
-      cy.get('#title').type('Test Title')
-      cy.get('#author').type('Test Author')
-      cy.get('#url').type('http://testurl.com')
-      cy.get('#createbutton').click()
-      cy.get('.info').contains('A new blog \'Test Title\' by \'Test Author\' added')
-      cy.get('.info').should('have.css', 'color', 'rgb(0, 128, 0)')
-      cy.wait(3100)
-      cy.get('.info').should('not.exist')
-      cy.root().find('.blog').should('have.length', 1)
-      cy.root().find('.blog').first().get('.blogHeader').contains('\'Test Title\' by \'Test Author\'')
-      cy.root().find('.blog').first().get('.blogHeader').contains('\'Test Title\' by \'Test Author\'')
-      cy.root().find('.blog').first().get('.viewbutton')
+      cy.contains('a', 'Blogs').click()
+      cy.contains('button', 'Create new').click()
+      cy.contains('Create Blog').should('be.visible')
+      cy.contains('button', 'Cancel').click()
+      cy.contains('Blogs').should('be.visible')
+      cy.contains('button', 'Create new').click()
+      cy.get('input[name="title"]').type('Test Title')
+      cy.get('input[name="author"]').type('Test Author')
+      cy.get('input[name="url"]').type('http://testurl.com')
+      cy.get('form').contains('button', 'Create').click()
+      cy.contains('A new blog \'Test Title\' by \'Test Author\' added').should('be.visible')
+      cy.wait(5100)
+      cy.contains('A new blog \'Test Title\' by \'Test Author\' added').should('not.be.visible')
+      cy.root().find('tr').should('have.length', 1)
+      cy.contains('tr', 'Test Title').should('be.visible')
     })
 
     it('Can logout', function () {
-      cy.get('#logoutbutton').click()
-      cy.get('#loginform')
+      cy.contains(`${testUser.name} logged in`).should('be.visible')
+      cy.contains('Logout').click()
+      cy.contains(`${testUser.name} logged in`).should('not.be.visible')
+      cy.contains('Welcome to blog application!').should('be.visible')
     })
   })
 
-  describe.only('When logged in with existing blogs', function () {
+  describe('Users', function () {
     beforeEach(function () {
       cy.login({ username: testUser.username, password: testUser.password })
-      cy.createBlog({ title: 'Test Title', author: 'Test Author', url: 'http://testurl.com' })
+      cy.createBlog(testBlog1)
       cy.login({ username: testUser2.username, password: testUser2.password })
-      cy.createBlog({ title: 'Other Title', author: 'Other Author', url: 'http://otherurl.com' })
+      cy.createBlog(testBlog2)
       cy.login({ username: testUser.username, password: testUser.password })
+      cy.contains('a', 'Users').click()
     })
 
-    it('A blog can be liked', function () {
-      cy.root().find('.blog').first().find('.viewbutton').click()
-      cy.root().find('.blog').first().find('.blogLikes').contains('0')
-      cy.root().find('.blog').first().find('.likebutton').click()
-      cy.root().find('.blog').first().find('.blogLikes').contains('1')
-      cy.root().find('.blog').first().find('.likebutton').click()
-      cy.root().find('.blog').first().find('.blogLikes').contains('2')
+    it('users are listed', function () {
+      cy.contains('Users').should('be.visible')
+      cy.contains(testUser.name).should('be.visible')
+      cy.contains(testUser2.name).should('be.visible')
+      cy.get('table').find('tr').its('length').should('eq', 2)
+      cy.contains('td', 'Blogs: 1').should('be.visible')
+    })
+
+    it('clicked users blogs are displayed', function () {
+      cy.contains('a', testUser.name).click()
+      cy.contains(`User: ${testUser.name}`).should('be.visible')
+      cy.contains(testBlog1.title).should('be.visible')
+    })
+
+    it('clicking blog title if used displays blog', function () {
+      cy.contains('a', testUser.name).click()
+      cy.contains('a', testBlog1.title).click()
+      cy.contains(`Blog: '${testBlog1.title}' by '${testBlog1.author}'`).should('be.visible')
+      cy.contains(testBlog1.url).should('be.visible')
+      cy.contains(testUser.name).should('be.visible')
+    })
+  })
+
+  describe('When logged in with existing blogs', function () {
+    beforeEach(function () {
+      cy.login({ username: testUser.username, password: testUser.password })
+      cy.createBlog(testBlog1)
+      cy.login({ username: testUser2.username, password: testUser2.password })
+      cy.createBlog(testBlog2)
+      cy.login({ username: testUser.username, password: testUser.password })
+      cy.contains('a', 'Blogs').click()
     })
 
     it('Can delete own blog', function () {
-      cy.root().find('.blog').first().find('.viewbutton').click()
-      cy.root().find('.blog').first().find('.deletebutton').click()
-      cy.contains('Test Title').should('not.exist')
+      cy.contains('a', testBlog1.title).click()
+      cy.contains('button', 'Delete').click()
+      cy.contains(`Deleted '${testBlog1.title}' by '${testBlog1.author}'`).should('be.visible')
+      cy.wait(5100)
+      cy.contains(`Deleted '${testBlog1.title}' by '${testBlog1.author}'`).should('not.be.visible')
+      cy.contains(testBlog1.title).should('not.be.visible')
     })
 
     it('Cannot delete others blog', function () {
-      cy.root().find('.blog').last().find('.viewbutton').click()
-      cy.root().find('.blog').last().find('.deletebutton').should('not.exist')
+      cy.contains('a', testBlog2.title).click()
+      cy.contains('button', 'Delete').should('not.be.visible')
     })
 
     it('blogs are ordered by like', function () {
-      cy.root().find('.blog').first().find('.viewbutton').click()
-      cy.root().find('.blog').first().find('.likebutton').click()
-      cy.root().find('.blog').first().find('.blogLikes').contains('1')
-      cy.root().find('.blog').first().find('.blogLikes')
-      cy.root().find('.blog').last().find('.viewbutton').click()
-      cy.root().find('.blog').last().find('.likebutton').click()
-      cy.root().find('.blog').last().find('.blogLikes').contains('1')
-      cy.root().find('.blog').last().find('.likebutton').click()
-      cy.root().find('.blog').first().get('.blogHeader').contains('Other Title')
+      cy.contains('a', testBlog1.title).click()
+      cy.get('button').contains('Like').click()
+      cy.contains('a', 'Blogs').click()
+      cy.root().find('tr').first().contains(testBlog1.title)
+      cy.contains('a', testBlog2.title).click()
+      cy.get('button').contains('Like').click()
+      cy.get('button').contains('Like').click()
+      cy.contains('a', 'Blogs').click()
+      cy.root().find('tr').first().contains(testBlog2.title)
+    })
+  })
+
+  describe('When logged out with existing blogs', function () {
+    beforeEach(function () {
+      cy.login({ username: testUser.username, password: testUser.password })
+      cy.createBlog(testBlog1)
+      cy.contains('Logout').click()
     })
 
+    it('Can comment on blog', function () {
+      cy.contains('a', 'Blogs').click()
+      cy.contains('a', testBlog1.title).click()
+      cy.get('input[name="comment"]').type('comment 1')
+      cy.get('form').contains('button', 'Add').click()
+      cy.contains('comment 1').should('be.visible')
+      cy.get('input[name="comment"]').clear().type('comment 2')
+      cy.get('form').contains('button', 'Add').click()
+      cy.contains('comment 2').should('be.visible')
+    })
   })
 })
